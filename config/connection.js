f96217bf33082093ec779fcc40b55e32eb185b67
JakeL501 <jacob.letourneau@my.colby-sawyer.edu>
2018-02-15 00:14:33 -0500

    lots of code

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+// Local Database Configuration with Mongoose
+mongoose.connect("mongodb://localhost/goodnews", function(error)
+	{if(error) throw error;
+	console.log("Database connected");
+});
+
+// mLab database
+// mongoose.connect("mongodb://heroku_1hbwwwc5:vou3nt74ijraqkecfu8t9q32oi@ds231758.mlab.com:31758/heroku_1hbwwwc5", function(err) {
+// 	if(err) throw err;
+// 	console.log('database connected');
+// });
